1e90a19ede2c0982e2f39ffffc0d9fc84ffa14e2
Jayson Cleofas <jayson.cleofas@scrubbed.net>
2025-05-22 06:12:20 +0800

    Install octane

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -132,5 +132,14 @@
       })
     })
   </script>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-06SJGFQCVN"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-06SJGFQCVN');
+  </script>
 </body>
 </html>
