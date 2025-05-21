20e599e15fd9b2e073f8baa520ecb21f38fde2ac
Jayson Cleofas <jayson.cleofas@scrubbed.net>
2025-05-22 06:15:21 +0800

    Add google tag

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,15 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title') {{ get_setting('siteTitle') }}</title>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-06SJGFQCVN"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-06SJGFQCVN');
+  </script>
   @vite('resources/css/app.css')
   @livewireStyles
 </head>
@@ -131,15 +140,6 @@
         submitBtn.textContent = 'Submit'
       })
     })
-  </script>
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-06SJGFQCVN"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-06SJGFQCVN');
   </script>
 </body>
 </html>
