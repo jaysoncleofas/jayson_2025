b15892793a885970b144b8475106ece4ee236d27
Jayson Cleofas <jayson.cleofas@scrubbed.net>
2025-05-29 03:02:17 +0800

    Rearange work and contact in the home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -66,14 +66,6 @@ $projects = Project::orderBy('title')->get();
             @endforelse
           </div>
           <div class="space-y-10 lg:pl-16 xl:pl-24">
-            <form action="/thank-you" class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-              <h2 class="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100"><svg viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class="h-6 w-6 flex-none">
-                <path d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z" class="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"></path>
-                <path d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6" class="stroke-zinc-400 dark:stroke-zinc-500"></path>
-              </svg><span class="ml-3">Message me</span></h2>
-              <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">I’d love to hear from you. Hit the button below to drop me a message — I’ll get back to you as soon as I can.</p>
-              <div class="mt-6"><a href="/contact" class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70 flex-none w-full" type="submit">Contact</a></div>
-            </form>
             <div class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
               <h2 class="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
                 <svg viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class="h-6 w-6 flex-none">
@@ -112,6 +104,15 @@ $projects = Project::orderBy('title')->get();
                 </svg>
               </a>
             </div>
+
+            <form action="/thank-you" class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+              <h2 class="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100"><svg viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class="h-6 w-6 flex-none">
+                <path d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z" class="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"></path>
+                <path d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6" class="stroke-zinc-400 dark:stroke-zinc-500"></path>
+              </svg><span class="ml-3">Contact me</span></h2>
+              <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">I’d love to hear from you. Hit the button below to drop me a message — I’ll get back to you as soon as I can.</p>
+              <div class="mt-6"><a href="/contact" class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70 flex-none w-full" type="submit">Contact</a></div>
+            </form>
           </div>
         </div>
       </div>
